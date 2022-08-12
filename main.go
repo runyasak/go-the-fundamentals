49eae00be67fa4c7ee99f332521fbdf041823b4e
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2022-08-13 00:20:55 +0700

    feat: add sumOfFirst and isPrime

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ func main() {
 	fmt.Println(greetingWithAge("Pallat", 30))
 	fmt.Println(greetingWithAgeAndDrink("Pallat", 30, "Cola"))
 	fmt.Println(isOdd(1))
+	fmt.Println(sumOfFirst(3))
+	fmt.Println(isPrime(1))
+	fmt.Println(isPrime(2))
+	fmt.Println(isPrime(3))
+	fmt.Println(isPrime(4))
 }
 
 
@@ -35,4 +40,31 @@ func isOdd(n int) bool {
 	}
 
 	return false
+}
+
+// It should return sum of n, n-1, n-2, ..., 1
+// sumOfFirst(3) should return 3+2+1=6
+func sumOfFirst(n int) int {
+	result := 0
+
+	for i := 0; i < n; i++ {
+		result += n - i
+	}
+
+	return result
+}
+
+
+// A prime number is a number greater than 1 with only two factors – themselves and 1
+func isPrime(n int) bool {
+	count := 0
+
+	for i := 2; i <= n/2; i++ {
+			if n % i == 0 {
+					count++
+					break
+			}
+	}
+
+	return count == 0 && n != 1
 }
