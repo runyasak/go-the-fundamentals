612dcd669ed25360d2f6472c76514bfb4343720f
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2022-08-12 23:53:23 +0700

    feat: add isOdd

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	fmt.Println(greeting("Pallat"))
 	fmt.Println(greetingWithAge("Pallat", 30))
 	fmt.Println(greetingWithAgeAndDrink("Pallat", 30, "Cola"))
+	fmt.Println(isOdd(1))
 }
 
 
@@ -26,4 +27,12 @@ func greetingWithAge(name string, age uint) string {
 // greetingWithAgeAndDrink("Pallat", 30, "Cola") should return "Hello, Pallat. You are 30 years old and your favorite drink is Cola."
 func greetingWithAgeAndDrink(name string, age uint, drink string) string {
 	return "Hello, " + name + ". You are " + strconv.FormatUint(uint64(age), 10) + " years old and your favorite drink is " + drink + "."
+}
+
+func isOdd(n int) bool {
+	if result := n % 2 == 1; result {
+		return true
+	}
+
+	return false
 }
